7a8ab7a73c268ce2f717762b8aa793cfa263700b
Ian-Harreschou <harresci@oregonstate.edu>
2023-11-26 15:27:12 -0800

    test formula for amortization

--- src/loans.jl
+++ src/loans.jl
@@ -1,1 +1,9 @@
 # This file contains functions that pertain to loans
+
+function amortization(P::Float64, i::Float64, n=12)
+
+        A = P * (i * (1 + i) ^ n) / ((1 + i) ^ n - 1)
+
+        return A, n
+        
+end
