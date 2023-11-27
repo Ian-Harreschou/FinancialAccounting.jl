1fd734eef8084dfc2af7f7ebc390eebabf8357c9
Ian-Harreschou <harresci@oregonstate.edu>
2023-11-26 19:26:04 -0800

    adding basic amortization functionality

--- src/loans.jl
+++ src/loans.jl
@@ -1,9 +1,36 @@
 # This file contains functions that pertain to loans
 
-function amortization(P::Float64, i::Float64, n=12)
+amortization_periods = Dict("yearly" => 1, 
+								"semi-annually" => 2, 
+								"quarterly" => 4,
+								"monthly" => 12, 
+								)
 
-        A = P * (i * (1 + i) ^ n) / ((1 + i) ^ n - 1)
+function amortization(P::Union{Float64,Int64},
+    i::Union{Float64,Int64},
+    compound_frequency::String, 
+    term_length::Int
+    )
 
-        return A, n
-        
+# A = periodic payment amount
+# P = remaining principal on the loan, subtracting any down payments
+# i = periodic percentage rate (0 <= i <= 1)
+# n = total number of payments
+
+n = get(amortization_periods, compound_frequency, "NOT FOUND") * term_length
+
+if compound_frequency == "yearly"
+i = i
+else	
+i = i/12
+end
+
+if i == 0
+A = P / n
+else
+A = P * (i + i / ((1 + i) ^ n - 1))
+end
+
+return round(A, sigdigits = 4)
+
 end
