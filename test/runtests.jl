e9fdb4c10f03d0e9fe350d7e1bd8fe3ab2df85d4
Ian-Harreschou <harresci@oregonstate.edu>
2023-11-26 21:53:25 -0800

    troubleshooting

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using FinancialAccounting
 using Test
 
 @testset "FinancialAccounting.jl" begin
-    @test FinancialAccounting.jl.hello_package() == "Hello FinancialAccounting!"
-    @test FinancialAccounting.jl.hello_package() != "Hello World!"
+    @test FinancialAccounting.hello_package() == "Hello FinancialAccounting!"
+    @test FinancialAccounting.hello_package() != "Hello World!"
 end
